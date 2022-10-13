5815125b95f7396786226f32c2610a9cec4b5f0f
Robiul Hossain <59972522+rhrashal@users.noreply.github.com>
2022-10-13 18:42:59 +0600

    inital upload

---
+++ test/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Sign = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Sign
+            // 
+            this.Sign.Location = new System.Drawing.Point(55, 83);
+            this.Sign.Name = "Sign";
+            this.Sign.Size = new System.Drawing.Size(129, 48);
+            this.Sign.TabIndex = 0;
+            this.Sign.Text = "Sign";
+            this.Sign.UseVisualStyleBackColor = true;
+            this.Sign.Click += new System.EventHandler(this.Sign_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(55, 173);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(292, 115);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(218, 83);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(129, 48);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 345);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Sign);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Sign;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ test/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Drawing.Imaging;
+using System.Security.Cryptography;
+using System.Reflection;
+using System.IO;
+//using FP430SAPI;
+
+namespace test
+{
+    //unsafe delegate void MemCpyImpl(byte* src, byte* dest, int len);
+    public partial class Form1 : Form
+    {
+        [DllImport("FP430S.dll", EntryPoint = "Lib_GetSignature", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]   // 
+        unsafe public static extern int Lib_GetSignature(int encryptType, byte* outputData, int timeout);
+
+        [DllImport("FP430S.dll", EntryPoint = "USB_Open", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern int USB_Open();
+
+        [DllImport("FP430S.dll", EntryPoint = "USB_Close", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern int USB_Close();
+
+        [DllImport("FP430S.dll", EntryPoint = "Lib_Alignment", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern int Lib_Alignment(int timeout);
+
+        [DllImport("FP430S.dll", EntryPoint = "Lib_TransPic", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern int Lib_TransPic(string srcPic, string dstPic);
+
+
+        Image GuestImage;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        unsafe private void Sign_Click(object sender, EventArgs e)
+        {
+            int ret = USB_Close();
+            ret = USB_Open();
+
+            byte[] signData = new byte[1024 * 400];
+
+
+            fixed (byte* p = &signData[0])
+            {
+                ret = Lib_GetSignature(0, p, 60000);
+            }
+
+
+            int picLen;
+            int dataLen;
+
+            byte[] dataBuf;
+            byte[] picBuf;
+
+            string basePah;
+
+
+            dataLen = signData[0] * 256 * 256 + signData[1] * 256 + signData[2];
+            picLen = signData[3] * 256 * 256 + signData[4] * 256 + signData[5];
+
+            picBuf = new byte[picLen];
+            dataBuf = new byte[dataLen];
+
+            Array.ConstrainedCopy(signData, 6, dataBuf, 0, dataLen);
+            Array.ConstrainedCopy(signData, 6 + dataLen, picBuf, 0, picLen);
+
+
+            basePah = System.Environment.CurrentDirectory;
+          
+
+
+            pictureBox1.BackgroundImage = ByteToImage(picBuf);
+            GuestImage = ByteToImage(picBuf);
+           
+        }
+
+        public Image ByteToImage(byte[] byteArrayIn)
+        {
+
+
+            MemoryStream ms = new MemoryStream(byteArrayIn);
+            Image returnImage = Image.FromStream(ms);
+            return returnImage;
+        }
+
+        public bool BinaryToFile(Byte[] byTmp, string path)
+        {
+            FileStream fs;
+            BinaryWriter bwr;
+
+            fs = new FileStream(path, FileMode.Create);
+            bwr = new BinaryWriter(fs);
+
+            bwr.Write(byTmp);
+            bwr.Flush();
+
+            bwr.Close();
+            fs.Close();
+            fs.Dispose();
+
+            return true;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            byte[] img = ImageToByte(GuestImage);
+
+        }
+
+        public byte[] ImageToByte(Image imageIn)
+        {
+
+            MemoryStream ms = new MemoryStream();
+            imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+            return ms.ToArray();
+        }
+
+
+    }
+}
